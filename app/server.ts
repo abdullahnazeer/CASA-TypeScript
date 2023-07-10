df9d067f618aa559ba0146bdf4c7e05c36c7af6f
theo.selenskiy <theo.selenskiy@engineering.digital.dwp.gov.uk>
2023-07-10 09:54:58 +0100

    add comments

--- app/server.ts
+++ app/server.ts
@@ -7,11 +7,17 @@ import app from "./app";
 const expressApp = express();
 expressApp.use(noSniff());
 
-// we have to consider where these assets will live relative to the transpiled JS
+/**
+ * we have to consider where these assets will live relative to the transpiled JS
+ * 
+ * for example, in the gulpfile the 'img' task moves all required images to '/dist/public/images'
+ * this file (server.ts) will be transpiled into 'server.js' and will be located in '/dist/server.js'
+ * now all the images are accessible to 'server.js' with the relative import './public/images' 
+ */
 expressApp.use('/assets/js', express.static(path.resolve(__dirname, './public/javascript')));
 expressApp.use('/assets/images', express.static(path.resolve(__dirname, './public/images')));
 expressApp.use('/assets/fonts', express.static(path.resolve(__dirname, './public/fonts')));
-expressApp.use(express.static(path.resolve(__dirname, './public')));
+expressApp.use('/css', express.static(path.resolve(__dirname, './public/css')));
 
 const casaApp = app();
 
